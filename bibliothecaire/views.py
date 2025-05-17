edb876809ffd5363b274bb79f9e59db6a1dff6be
julien <julienchauvin@hotmail.fr>
2025-05-17 20:48:22 +0200

    création d'un accueil bibliothecaire ainsi que l'ajour d'un livre

--- bibliothecaire/views.py
+++ bibliothecaire/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Livre, Cd, Dvd, Jeux_de_plateau
+from .forms import LivreForm
 
 # Create your views here.
+
+
+def accueil_bibliothecaire(request):
+    form = LivreForm()
+
+    if request.method == 'POST':
+        form = LivreForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("bibliothecaire:accueil_bibliothecaire")
+        
+    else:
+        form = LivreForm()
+
+
+    context = {"livre" : Livre.objects.all(),
+               "dvd" : Dvd.objects.all(),
+               "cd" : Cd.objects.all(),
+               "jeux_de_plateau" : Jeux_de_plateau.objects.all(),
+               "form": form}
+    
+    return render(request, 'accueil_bibliothecaire.html', context)
+
+
